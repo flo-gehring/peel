d62582312432d555d23b4c8ab05dac8ac2077dd0
Flo Gehring <florian.gehring@pm.me>
2025-12-20 20:50:03 +0100

    EPIC-3: Scoping - Exceptions and runtime tests

--- src/test/java/de/flogehring/peel/run/TernaryOperatorTest.java
+++ src/test/java/de/flogehring/peel/run/TernaryOperatorTest.java
@@ -2,8 +2,10 @@ package de.flogehring.peel.run;
 
 import de.flogehring.peel.core.values.PeelValue;
 import de.flogehring.peel.parse.PeelGrammar;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static de.flogehring.peel.core.values.PeelValue.integer;
 import static de.flogehring.peel.run.TestHelpers.runProgrammAndExpect;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
@@ -56,5 +58,110 @@ public class TernaryOperatorTest {
                     var result = 10 + (x == 1 ? 5 : 0);
                     result;
                 """, PeelValue.integer(15));
+    }
+
+    @Nested
+    class Scoping {
+
+        @Test
+        void ternaryConditionCanAccessOuterVariables() {
+            runProgrammAndExpect("""
+                        var x = 5;
+                        var y = 10;
+                        var result = x == 5 ? y : 0;
+                        result;
+                    """, integer(10));
+        }
+
+        @Test
+        void ternaryBranchesCanAccessOuterVariables() {
+            runProgrammAndExpect("""
+                        var a = 100;
+                        var b = 200;
+                        var result = 1 == 1 ? a : b;
+                        result;
+                    """, integer(100));
+        }
+
+        @Test
+        void ternaryWithComplexExpressionsInBranches() {
+            runProgrammAndExpect("""
+                        var x = 5;
+                        var y = 10;
+                        var result = 1 == 1 ? x + y : x - y;
+                        result;
+                    """, integer(15));
+        }
+
+        @Test
+        void ternaryWithVariablesFromDifferentScopes() {
+            runProgrammAndExpect("""
+                        var outer = 5;
+                        if (1 == 1) {
+                            var inner = 10;
+                            var result = outer == 5 ? inner : 0;
+                            result;
+                        }
+                    """, integer(10));
+        }
+
+        @Test
+        void ternaryInLoopWithScopedVariables() {
+            runProgrammAndExpect("""
+                        var sum = 0;
+                        for (i in [1, 2, 3, 4, 5]) {
+                            var toAdd = i == 3 ? 100 : i;
+                            sum = sum + toAdd;
+                        }
+                        sum;
+                    """, integer(112));
+        }
+
+        @Test
+        void ternaryAccessesVariablesFromOuterScopeInBothBranches() {
+            runProgrammAndExpect("""
+                        var trueValue = 100;
+                        var falseValue = 200;
+                        var condition = 1;
+                        var result = condition == 1 ? trueValue : falseValue;
+                        result;
+                    """, integer(100));
+        }
+
+        @Test
+        void ternaryInNestedIfStatement() {
+            runProgrammAndExpect("""
+                        var x = 5;
+                        if (1 == 1) {
+                            var y = 10;
+                            var result = x == 5 ? y : 0;
+                            result;
+                        }
+                    """, integer(10));
+        }
+
+        @Test
+        void ternaryInWhileLoopCondition() {
+            runProgrammAndExpect("""
+                        var counter = 0;
+                        var limit = 3;
+                        var result = 0;
+                        while (!(counter == (limit == 3 ? 3 : 5))) {
+                            result = result + 1;
+                            counter = counter + 1;
+                        }
+                        result;
+                    """, integer(3));
+        }
+
+        @Test
+        void ternaryWithVariableDeclarationInOuterScope() {
+            runProgrammAndExpect("""
+                        var x = 5;
+                        var choice = 1;
+                        var result = choice == 1 ? x + 10 : x - 10;
+                        result;
+                    """, integer(15));
+        }
     }
 }
--- src/test/java/de/flogehring/peel/run/WhileLoopTest.java
+++ src/test/java/de/flogehring/peel/run/WhileLoopTest.java
@@ -2,11 +2,15 @@ package de.flogehring.peel.run;
 
 import de.flogehring.peel.core.values.None;
 import de.flogehring.peel.core.values.PeelValue;
+import de.flogehring.peel.run.exceptions.UndefinedVarException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
 import java.util.concurrent.TimeUnit;
 
+import static de.flogehring.peel.core.values.PeelValue.integer;
+import static de.flogehring.peel.run.TestHelpers.expectErrorOnRun;
 import static de.flogehring.peel.run.TestHelpers.runProgrammAndExpect;
 
 @Timeout(value = 5, unit = TimeUnit.SECONDS)
@@ -40,5 +44,163 @@ public class WhileLoopTest {
                         """,
                 PeelValue.integer(2)
         );
+    }
+
+    @Nested
+    class Scoping {
+
+        @Test
+        @Timeout(2)
+        void variablesDeclaredInWhileLoopDoNotLeak() {
+            String programm = """
+                    var x = 0;
+                    while (!(x == 3)) {
+                        var temp = x + 1;
+                        x = temp;
+                    }
+                    temp;
+                    """;
+            expectErrorOnRun(programm, UndefinedVarException.class);
+        }
+
+        @Test
+        @Timeout(2)
+        void whileLoopCanModifyOuterScopeVariables() {
+            runProgrammAndExpect(
+                    """
+                                var sum = 0;
+                                var counter = 0;
+                                while (!(counter == 5)) {
+                                    sum = sum + counter;
+                                    counter = counter + 1;
+                                }
+                                sum;
+                            """,
+                    integer(10)
+            );
+        }
+
+        @Test
+        @Timeout(2)
+        void whileLoopCanShadowOuterVariable() {
+            runProgrammAndExpect(
+                    """
+                                var x = 100;
+                                var counter = 0;
+                                while (!(counter == 3)) {
+                                    var x = counter;
+                                    counter = counter + 1;
+                                }
+                                x;
+                            """,
+                    integer(100)
+            );
+        }
+
+        @Test
+        @Timeout(2)
+        void nestedWhileLoopsWithShadowing() {
+            runProgrammAndExpect(
+                    """
+                                var result = 0;
+                                var i = 0;
+                                while (!(i == 2)) {
+                                    var j = 0;
+                                    while (!(j == 2)) {
+                                        var i = 10;
+                                        result = i + j;
+                                        j = j + 1;
+                                    }
+                                    i = i + 1;
+                                }
+                                result;
+                            """,
+                    integer(11)
+            );
+        }
+
+        @Test
+        @Timeout(2)
+        void nestedWhileLoopInnerVariableDoesNotLeak() {
+            String programm = """
+                    var i = 0;
+                    while (!(i == 2)) {
+                        var j = 0;
+                        while (!(j == 2)) {
+                            var innerVar = 42;
+                            j = j + 1;
+                        }
+                        i = i + 1;
+                    }
+                    innerVar;
+                    """;
+            expectErrorOnRun(programm, UndefinedVarException.class);
+        }
+
+        @Test
+        @Timeout(2)
+        void whileLoopBodyBlockCreatesScope() {
+            String programm = """
+                    var x = 0;
+                    while (!(x == 2)) {
+                        var blockVar = 5;
+                        x = x + 1;
+                    }
+                    blockVar;
+                    """;
+            expectErrorOnRun(programm, UndefinedVarException.class);
+        }
+
+        @Test
+        @Timeout(2)
+        void whileLoopWithIfStatementScoping() {
+            runProgrammAndExpect(
+                    """
+                                var result = 0;
+                                var counter = 0;
+                                while (!(counter == 3)) {
+                                    if (counter == 1) {
+                                        var matched = 100;
+                                        result = matched;
+                                    }
+                                    counter = counter + 1;
+                                }
+                                result;
+                            """,
+                    integer(100)
+            );
+        }
+
+        @Test
+        @Timeout(2)
+        void whileLoopConditionCanAccessOuterVariables() {
+            runProgrammAndExpect(
+                    """
+                                var limit = 5;
+                                var counter = 0;
+                                while (!(counter == limit)) {
+                                    counter = counter + 1;
+                                }
+                                counter;
+                            """,
+                    integer(5)
+            );
+        }
+
+        @Test
+        @Timeout(2)
+        void multipleVariablesInWhileLoopScope() {
+            String programm = """
+                    var x = 0;
+                    while (!(x == 2)) {
+                        var a = 1;
+                        var b = 2;
+                        var c = 3;
+                        x = x + 1;
+                    }
+                    a;
+                    """;
+            expectErrorOnRun(programm, UndefinedVarException.class);
+        }
     }
 }
